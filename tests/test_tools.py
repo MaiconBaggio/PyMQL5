1c2abc7ceae2321932c99a3a1a2ce64cae787a2c
Valmir Franca <vfranca3@gmail.com>
2019-12-20 05:01:29 -0300

    Substituidos formato de testes pytest para unittest

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
+from unittest import TestCase
 from PyMQL5.Tools import MQL5Period as period
 
 
-def test_formata_m1():
-    assert period("m1") == "PERIOD_M1"
-    assert period("M1") == "PERIOD_M1"
+class ToolsTestCase(TestCase):
 
-def test_formata_periodo_para_algo_aleatorio():
-    assert period("aleatorio") == "PERIOD_CURRENT"
+    def test_formata_m1(self):
+        self.assertEqual(period("m1"), "PERIOD_M1")
+        self.assertEqual(period("M1"), "PERIOD_M1")
 
-def test_formata_periodo_para_uma_string_vazia():
-    assert period("") == "PERIOD_CURRENT"
+    def test_formata_periodo_para_algo_aleatorio(self):
+        self.assertEqual(period("aleatorio"), "PERIOD_CURRENT")
+
+    def test_formata_periodo_para_uma_string_vazia(self):
+        self.assertEqual(period(""), "PERIOD_CURRENT")
